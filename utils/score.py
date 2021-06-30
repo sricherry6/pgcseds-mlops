fc27b44983260add3cc904810561c643fb4e8bdf
akshatcx <akshat.c2k@gmail.com>
2021-06-30 23:14:39 +0530

    [update] requirements

--- utils/score.py
+++ utils/score.py
@@ -1,8 +1,5 @@
 from collections import Counter
-from numpy import mean, std
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
