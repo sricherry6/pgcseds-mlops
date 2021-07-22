cd202d28f60b0ff5e3b5859a26adb967d454267b
sricherry6 <priyanka.chandrabhatla@gmail.com>
2021-07-22 23:52:33 +0530

    cicd workflow

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-from ml_utils import load_model, predict
+from ml_utils import load_model, predict, retrain
 from datetime import date,datetime
 
 app = FastAPI(
